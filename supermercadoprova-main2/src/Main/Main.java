6404debadb1f3ccacb6255890bd73372cd3e2d90
Guilherme Ovalhe <97913632+Guiovalhe@users.noreply.github.com>
2023-02-07 14:40:33 -0300

    Adding updates

---
+++ supermercadoprova-main2/src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//TODO arrumar a baixa do estoque para quando nao tiver estoque o suficiente (bug)						  > linha 21  do Pedido e linha 81 do Estoque
+//TODO criar metodo para receber o valor pago e calcular o troco (funcionalidade)						  > linha 109 do Pedido
+//TODO criar metodo que defina a menor quantidade de notas possivel para o troco (funcionalidade dificil) > linha 109 do Pedido
+//      Notas possiveis = 100,50,20,10,5,2,1,0.5,0.25,0.1,0.05 e 0.01;
+        Menu.controleMenu();
+    }
+}
